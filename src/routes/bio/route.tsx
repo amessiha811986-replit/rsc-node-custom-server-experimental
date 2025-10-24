e05f8abddbc9a818cb288c79105585bc7d51f35d
amessiha811986-replit <alexander.messiha@outlook.com>
2025-10-24 11:18:27 -0400

    Update #2

--- src/routes/bio/route.tsx
+++ src/routes/bio/route.tsx
@@ -4,13 +4,13 @@ import { useRouteLoaderData } from "react-router";
 import type { BioLoader } from "../../vite-env.d";
 
 export default function Component() {
-  const { data } = useRouteLoaderData<ReturnType<BioLoader>>("id") as ReturnType<BioLoader>;
+  const { data } = useRouteLoaderData<ReturnType<BioLoader>>("bio") as ReturnType<BioLoader>;
   
   return (
     <main className="mx-auto max-w-screen-xl px-4 py-8 lg:py-12">
       <article className="prose mx-auto">
         <h1>Message:</h1>
-        <p>{data}</p>
+        <p>{data === "" ? "Hello, World" : data}</p>
       </article>
     </main>
   )
